4af31a832f5671c70a316c4b73410a196cf1c701
BrunoItess <brunitosi123@gmail.com>
2025-10-13 13:54:34 -0600

    Repositorio de ASYSC de scripts de la unidad 2

---
+++ Unidad_2_Series_de_Fourier/hola.py
@@ -0,0 +1,33 @@
+"""
+    ITESS TICS TI-501
+    Análisis de señales y sistemas de comunicaciones
+    Javier Montecillo
+    Escribiendo primeras líneas de código de python
+    Vázquez Coronado Nam_chul Bruno
+    09/10/2025
+"""
+
+print("¡Hola mundo!");
+
+#Usar python como una calculadora
+#suma
+print("Suma: 2 + 2 = ", 2 + 2);
+
+#resta
+print("Resta: 2 - 2 = ", 2 - 2);
+
+#multiplicación
+print("Multiplicación: 2 * 2 = ", 2 * 2);
+
+#potencia **
+print("Potencia: 2 ** 3 = ", 2 ** 3);
+
+#división
+print("División: 5 / 2 = ", 5 / 2);
+
+#div //
+print("División entera: 5 // 2 = ", 5 // 2);
+
+#módulo %
+print("Módulo: 5 % 2 = ", 5 % 2);
+
